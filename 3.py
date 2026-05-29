b9fc3ce8639a434b6ff10da384479546ac339f08
Pupkin512 <pupkin123@tutamail.com>
2026-05-29 23:56:27 +0300

    3.py

--- 3.py
+++ 3.py
@@ -1,12 +1,12 @@
 import asyncio
 async def sol():
-    counter = 0
+    coun = 0
     lock = asyncio.Lock()
     async def proc():
-        nonlocal counter
+        nonlocal coun
         async with lock:
             await asyncio.sleep(0.01)
-            counter = counter + 1
+            coun = coun + 1
     tasks = [proc(i) for i in range(100)]
     await asyncio.gather(*tasks)
-    print(f"Итоговое значение счетчика: {counter}")
+    print(f"Итоговое значение счетчика: {coun}")
